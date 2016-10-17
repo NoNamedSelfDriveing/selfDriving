cbb936dc0d1e9c6904785ec75f0169900fa4d518
evtktma <evtktma@gmail.com>
2016-10-18 00:49:24 +0900

    Add msg to send

--- com/src/sms/Manager.py
+++ com/src/sms/Manager.py
@@ -1,7 +1,9 @@
 import os
+import time
 from sendsms import sms
 
 FNAME = './sms/manager/phone_book'
+MSG = '모방 자율 주행 모드를 시작합니다.'
 
 class Manager(object):
     def __init__(self, ID, pw, sender):
@@ -20,4 +22,4 @@ class Manager(object):
     def sendSMS(self):
         self.getFileInfo()
         for who in self.receiver:
-            self.sms.sms(self.identity, self.password, self.sender, who)
+            self.sms.sms(self.identity, self.password, self.sender, who, time.ctime() + MSG)
